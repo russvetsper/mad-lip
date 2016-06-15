ada54569f7efbd2e7004f9b6514e211ba5a99c96
Andrew Niekamp, Quinton Williams and Shokouh Farvid <andrew+quinton+shokouh@>
2016-06-15 11:44:11 -0700

    Add the JS to Append temporary words

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(){
+    $(".person1").append("blahblah");
+    $(".person2").append("blahblah");
+    $(".animal").append("blahblah");
+    $(".exclamation").append("blahblah");
+    $(".verb").append("blahblah");
+    $(".noun").append("blahblah");
+
+    $("#story").show();
+
+
+  });
+});
